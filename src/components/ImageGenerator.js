fcae3f9b932188d2853247e373c45e10e6937203
Alex de Vries <alexthevries@gmail.com>
2023-12-21 17:03:53 +0100

    commit new version

--- src/components/ImageGenerator.js
+++ src/components/ImageGenerator.js
@@ -1,51 +1,58 @@
+import React, { useState } from "react";
+import OpenAI from "openai";
 import '../App.css';
-import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
-
 
 function ImageGenerator() {
     const [prompt, setPrompt] = useState("");
     const [result, setResult] = useState("");
     const [loading, setLoading] = useState(false);
+    const [placeholder, setPlaceholder] = useState("Type your tattoo idea here...");
 
-    const [placeholder, setPlaceholder] = useState(
-        "Type your tattoo idea here..."
-    );
+    // Initialize OpenAI with your API key
+    //const openai = new OpenAI(process.env.REACT_APP_OPENAI_API_KEY);
+    const openai = new OpenAI({ apiKey: process.env.REACT_APP_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
 
-    const configuration = new Configuration({
-        apiKey: "apikeyhere",
-    });
 
-    const openai = new OpenAIApi(configuration);
 
     const generateImage = async () => {
-        setPlaceholder(`Tattoo for ${prompt}..`);
+        setPlaceholder(`Tattoo for ${prompt}...`);
         setLoading(true);
 
         try {
-            const res = await openai.createImage({
-                prompt: `line art tattoo. white background, white border, black lines: ${prompt}`,
-                n: 1,
-                size: "512x512",
+            const response = await fetch('https://api.openai.com/v1/images/generations', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+                },
+                body: JSON.stringify({
+                    prompt: `line art tattoo. white background, white border, black lines: ${prompt}`,
+                    n: 1,
+                    size: "512x512",
+                })
             });
 
+            const data = await response.json();
             setLoading(false);
-            setResult(res.data.data[0].url);
+            if (data.data && data.data.length > 0) {
+                setResult(data.data[0].url);
+            } else {
+                throw new Error("No image data returned");
+            }
         } catch (error) {
             setLoading(false);
-            console.error(`Error generating image: ${error.response.data.error.message}`);
+            console.error(`Error generating image: ${error.message}`);
         }
     };
+
+
     return (
         <div className="container">
             {loading ? (
-                <>
-                    <h3>Generating tattoo, please wait...</h3>
-                </>
+                <h3>Generating tattoo, please wait...</h3>
             ) : (
                 <>
-                    <h2>AI overlord deciding your tattoo fate</h2>
-
+                    <h2>AI Overlord Deciding Your Tattoo Fate</h2>
                     <div className="input-wrapper">
                         <textarea
                             className="app-input"
@@ -54,19 +61,16 @@ function ImageGenerator() {
                             rows="1"
                             cols="50"
                         />
-                        <button onClick={generateImage}>Generate tattoo</button>
+                        <button onClick={generateImage}>Generate Tattoo</button>
                     </div>
 
-                    {result.length > 0 ? (
-                        <img className="result-image" src={result} alt="result" />
-                    ) : (
-                        <>
-                        </>
+                    {result.length > 0 && (
+                        <img className="result-image" src={result} alt="Generated Tattoo" />
                     )}
                 </>
             )}
         </div>
-    )
+    );
 }
 
-export default ImageGenerator
+export default ImageGenerator;
