f49960caf37ffeb539911ef3c04e5dbfbeb5a24c
Alex de Vries <alexthevries@gmail.com>
2023-12-22 20:56:22 +0100

    add password protection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
+import React, { useState } from 'react';
 import './App.css';
 import Tarotgen from './components/tarotreading';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && password === 'alexdevri.es') {
+      setIsAuthenticated(true);
+    }
+  };
+
   return (
     <div className="App">
+      {/* Main Content */}
       <header className="App-header">
         <Tarotgen />
       </header>
       <footer className="App-footer">
         made by <a href="https://www.alexdevri.es" target="_blank" rel="noopener noreferrer">alexdevri.es</a>
       </footer>
+
+      {/* Password Overlay */}
+      {!isAuthenticated && (
+        <div className="password-overlay">
+          <input
+            type="password"
+            placeholder="Enter Password.."
+            value={password}
+            onChange={handlePasswordChange}
+            onKeyDown={handleKeyDown}
+            className="password-input"
+          />
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
