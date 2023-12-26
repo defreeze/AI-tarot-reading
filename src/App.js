c641a1c21148946765994b6c902b1f6e362c7066
Alex de Vries <alexthevries@gmail.com>
2023-12-26 17:48:19 +0100

    select Ai based on tarot type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Tarotgen from './components/tarotreading';
+import { preloadImages } from './preloadImages';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
@@ -9,26 +10,10 @@ function App() {
   const Value = process.env.REACT_APP_KEY;
   const CorrectValue = Correct + Value;
   const [loading, setLoading] = useState(false);
+  const [choice, setChoice] = useState("");
 
   useEffect(() => {
-    const adjustStarPosition = () => {
-      const bottomRightImage = document.querySelector('.bottom-right-image');
-      const tarotStar = document.querySelector('.tarot-star');
-
-      if (bottomRightImage && tarotStar) {
-        const imageRect = bottomRightImage.getBoundingClientRect();
-        tarotStar.style.bottom = `${window.innerHeight - imageRect.bottom + (imageRect.height * 0.11)}px`;
-        tarotStar.style.right = `${window.innerWidth - imageRect.right + (imageRect.width * 0.55)}px`;
-      }
-    };
-
-    adjustStarPosition();
-    window.addEventListener('resize', adjustStarPosition);
-
-    // Cleanup function to remove the event listener
-    return () => {
-      window.removeEventListener('resize', adjustStarPosition);
-    };
+    preloadImages();
   }, []);
 
   const handlePasswordChange = (e) => {
@@ -41,18 +26,43 @@ function App() {
     }
   };
 
+
   return (
     <div className="App">
       {/* Main Content */}
 
-      <img src="/AI_tarot_final1.png" alt="AI Tarot" className="bottom-right-image" />
-      <img src="/AI_tarot_final1_purple.png" alt="Loading" className={`loading-image ${loading ? '' : 'hidden'}`} />
-      <img src="/tarotstar2.png" alt="Tarot Star" className="tarot-star" />
+      {choice === "1" || choice === "2" ? (
+        <>
+          <img src="/AI_tarot_final1_wise_woman.png" alt="AI Tarot" className="bottom-right-image-woman" />
+          <img src="/AI_tarot_final1_wise_woman_animation.png" alt="AI_female_animate" className="AI-woman-animate" />
+          <div className="center-container">
+            <img src="/spell-visual1.png" alt="loading_female_animate" className={`loading-visual-female ${loading ? '' : 'hidden'}`} />
+          </div>
+
+        </>
+      ) : choice === "3" || choice === "4" ? (
+        <>
+          <img src="/AI_tarot_final1_female.png" alt="AI Tarot" className="bottom-right-image-female" />
+          <img src="/AI_tarot_final1_female_animation.png" alt="AI_female_animate" className="AI-female-animate" />
+          <div className="center-container">
+            <img src="/spell-visual1.png" alt="loading_female_animate" className={`loading-visual-female ${loading ? '' : 'hidden'}`} />
+          </div>
+        </>
+      ) : choice === "" || choice === "5" || choice === "6" ? (
+        <>
+          <img src="/AI_tarot_final1.png" alt="AI Tarot" className="bottom-right-image" />
+          <img src="/AI_tarot_final1_purple.png" alt="Loading" className={`loading-image ${loading ? '' : 'hidden'}`} />
+          <img src="/tarotstar2.png" alt="Tarot Star" className="tarot-star" />
+        </>
+      ) : []
+      }
 
       <header className="App-header">
         <Tarotgen
           setIsAuthenticated={setIsAuthenticated}
           setLoading={setLoading}
+          choice={choice}
+          setChoice={setChoice}
           loading={loading}
         />      </header>
       <footer className="App-footer">
@@ -61,25 +71,27 @@ function App() {
 
 
       {/* Password Overlay */}
-      {!isAuthenticated && (
-        <div className="password-overlay">
-          <input
-            type="password"
-            placeholder="Enter Password..."
-            value={Value1}
-            onChange={handlePasswordChange}
-            onKeyDown={handleKeyDown}
-            className="password-input"
-          />
-          <p className="password-info">
-            ask for a password at&nbsp;
-            <a href="https://alexdevri.es/contact/" target="_blank" rel="noopener noreferrer">
-              www.alexdevri.es
-            </a>
-          </p>
-        </div>
-      )}
-    </div>
+      {
+        !isAuthenticated && (
+          <div className="password-overlay">
+            <input
+              type="password"
+              placeholder="Enter Password..."
+              value={Value1}
+              onChange={handlePasswordChange}
+              onKeyDown={handleKeyDown}
+              className="password-input"
+            />
+            <p className="password-info">
+              ask for a password at&nbsp;
+              <a href="https://alexdevri.es/contact/" target="_blank" rel="noopener noreferrer">
+                www.alexdevri.es
+              </a>
+            </p>
+          </div>
+        )
+      }
+    </div >
   );
 }
 
