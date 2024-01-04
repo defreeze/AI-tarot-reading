cf1ee2f3e84c551b963798e2d9125999af7e7cfd
Alex de Vries <alexthevries@gmail.com>
2024-01-04 19:07:02 +0100

    fix loading tarot names not loading

--- src/components/tarotcards.js
+++ src/components/tarotcards.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
 
 function TarotCards({ reading }) {
-    const [imagesLoaded, setImagesLoaded] = useState({ past: false, present: false, future: false });
+    //const [, setImagesLoaded] = useState({ past: false, present: false, future: false });
 
     const encodeFileName = (fileName) => {
         return fileName.split(' ').join('%20');
     };
 
-    const handleImageLoad = (card) => {
-        setImagesLoaded(prevState => ({ ...prevState, [card]: true }));
-    };
+    //const handleImageLoad = (card) => {
+    //    setImagesLoaded(prevState => ({ ...prevState, [card]: true }));
+    //};
 
-    useEffect(() => {
-        setImagesLoaded({ past: false, present: false, future: false });
-    }, [reading]);
+    //useEffect(() => {
+    //    setImagesLoaded({ past: false, present: false, future: false });
+    //}, [reading]);
 
     return (
         <div className="tarot-cards">
@@ -24,9 +24,9 @@ function TarotCards({ reading }) {
                             src={`/tarot_deck/${encodeFileName(reading.past.name)}.jpg`}
                             alt={reading.past.name}
                             className={reading.past.reversed ? 'reversed' : ''}
-                            onLoad={() => handleImageLoad('past')}
+                        //onLoad={() => handleImageLoad('past')}
                         />
-                        {imagesLoaded.past && <h5>
+                        {<h5>
                             {reading.past.name}
                             {reading.past.reversed && <div className="reversed-text"> Reversed</div>}
                         </h5>}
@@ -40,9 +40,9 @@ function TarotCards({ reading }) {
                             src={`/tarot_deck/${encodeFileName(reading.present.name)}.jpg`}
                             alt={reading.present.name}
                             className={reading.present.reversed ? 'reversed' : ''}
-                            onLoad={() => handleImageLoad('present')}
+                        //onLoad={() => handleImageLoad('present')}
                         />
-                        {imagesLoaded.present && <h5>
+                        {<h5>
                             {reading.present.name}
                             {reading.present.reversed && <div className="reversed-text"> Reversed</div>}
                         </h5>}
@@ -56,9 +56,9 @@ function TarotCards({ reading }) {
                             src={`/tarot_deck/${encodeFileName(reading.future.name)}.jpg`}
                             alt={reading.future.name}
                             className={reading.future.reversed ? 'reversed' : ''}
-                            onLoad={() => handleImageLoad('future')}
+                        //onLoad={() => handleImageLoad('future')}
                         />
-                        {imagesLoaded.future && <h5>
+                        {<h5>
                             {reading.future.name}
                             {reading.future.reversed && <div className="reversed-text"> Reversed</div>}
                         </h5>}
