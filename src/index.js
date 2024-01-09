310f33e5027765b1e951d8525b5a22d0f975afab
Alex de Vries <alexthevries@gmail.com>
2024-01-09 20:58:11 +0100

    remove recaptcha and create some new characters

--- src/index.js
+++ src/index.js
@@ -4,26 +4,16 @@ import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReCAPTCHA from 'react-google-recaptcha';
 
 // Create the root once with the container element
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-
-// Define a function to handle reCAPTCHA changes
-function onChange(value) {
-  console.log("Captcha value:", value);
-}
 
 // Render the app inside the root without specifying the container again
 root.render(
   <GoogleOAuthProvider clientId="619587058121-qunfd2tpdn3ntvcgm2l66puq5a65rq0t.apps.googleusercontent.com">
     <React.StrictMode>
       <App />
-      <ReCAPTCHA
-        sitekey="6LfMzUopAAAAANeVgFTynsOpp1a_LXINKT3TxUqK"
-        onChange={onChange}
-      />
     </React.StrictMode>
   </GoogleOAuthProvider>
 );
