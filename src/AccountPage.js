fefa243955551b3fe90e80886b4cb90cd5173f97
Alex de Vries <alexthevries@gmail.com>
2024-01-30 22:36:25 +0100

    stripe payment integration

--- src/AccountPage.js
+++ src/AccountPage.js
@@ -1,15 +1,38 @@
 // AccountPage.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './AccountPage.css';
 
 function AccountPage({ profile, logOut }) {
     const navigate = useNavigate();
+    const stripeButtonRef = useRef(null);
 
     useEffect(() => {
         if (!profile) {
             navigate('/'); // Redirect to home if not logged in
         }
+        const script = document.createElement('script');
+        script.src = "https://js.stripe.com/v3/buy-button.js";
+        script.async = true;
+        script.onload = () => {
+            stripeButtonRef.current.innerHTML = `
+            <div class="stripe-buttons-container">
+                <stripe-buy-button
+                buy-button-id="buy_btn_1OeOtJHT5oVjW8Cf06ub65LB"
+                publishable-key="pk_live_51OeNIGHT5oVjW8CfCamtlKEIh0Sgk0TaL4VyqCtIeoj7BVS1ozyp8D1LjgaVZqLUKyK1FOZ3C7g50ogUlIIWdccB00X41v6XyF"
+                >
+                </stripe-buy-button>
+                <stripe-buy-button
+                buy-button-id="buy_btn_1OeNxWHT5oVjW8CfHueYoAj1"
+                publishable-key="pk_live_51OeNIGHT5oVjW8CfCamtlKEIh0Sgk0TaL4VyqCtIeoj7BVS1ozyp8D1LjgaVZqLUKyK1FOZ3C7g50ogUlIIWdccB00X41v6XyF"
+                >
+                </stripe-buy-button>
+            </div>
+            `;
+        };
+        stripeButtonRef.current.appendChild(script);
+
+
     }, [profile, navigate]);
 
     const handleLogOut = () => {
@@ -47,6 +70,9 @@ function AccountPage({ profile, logOut }) {
                         <p className="centered-text">
                             We value your opinion! <a href="https://forms.gle/mYw64PTEUC3C8RsWA" target="_blank" rel="noopener noreferrer">share feedback here</a>
                         </p>
+                        {/* Stripe Payment Button */}
+                        <div ref={stripeButtonRef}></div>
+
                     </div>
                 </div>
 
