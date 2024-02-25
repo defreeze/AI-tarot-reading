79a9334286a771d8733493484be0e2de3a2f4e70
Alex de Vries <alexthevries@gmail.com>
2024-02-25 15:29:53 +0100

    improved prompt and figure out firebase deploy

--- src/components/generatePrompt_daily.ts
+++ src/components/generatePrompt_daily.ts
@@ -10,11 +10,10 @@ export function generatePrompt_daily(variables: {
     return `Using your deep knowledge of tarot and a touch of creativity, provide a unique reading for ${variables.NAMEHERE}, who is currently feeling ${variables.MOODHERE}. Draw upon the cards: feelings (${variables.pastCard}), health (${variables.presentCard}), and thoughts (${variables.futureCard}), considering ${variables.CONTEXTHERE}. Aim for a reading that is both insightful and imaginative, allowing for unconventional interpretations that resonate on a personal level.
 
     FORMAT:
-    Greeting: A personalized touch for ${variables.NAMEHERE}.
+    <A greeting with a personalized touch for ${variables.NAMEHERE}.>
 
-    Summary: A brief yet powerful overview of the week ahead, focusing on the synergy of the cards.
+    <A brief yet powerful overview of the day ahead, focusing on the synergy of the cards.>
 
-    Detailed Insights:
     Feelings: Interpretation of ${variables.pastCard}.
     Health: Interpretation of ${variables.presentCard}.
     Thoughts: Interpretation of ${variables.futureCard}.
